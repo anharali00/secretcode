82435b4067b8405461cc9cffb48a5917e87ac2cb
anharali00 <44951121+anharali00@users.noreply.github.com>
2025-09-15 21:28:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-md5s = ["c72b969efdbb0ec7a5c44ef967cb2d2c", 'b5b1fcf50f7ac9a16f6e4796c6f82a07', 'be1e0e2cfbcc0d3d8c26465dc50f7f77', 'dc647eb65e6711e155375218212b3964', 'bdfc54c30f65a0e2d6f98a535d34d6bc', 'fe01d67a002dfa0f3ac084298142eccd', '63a9f0ea7bb98050796b649e85481845', '21232f297a57a5a743894a0e4a801fc3'];
-solves = ['summer', 'eagle', 'vodka', 'enter', 'ninja', 'orange', 'root', 'admin'];
+md5s = ["96d9632f2951f1c312f2518648839e9a", '0800fc577294c34e0b28ad2839435945', '0cc175b9c0f1b6a831c399e269772661', '8843d7f92416211de9ebb963ff4ce281', 'c9d19f273d6c3f61b20f3c8a582a4ad9'];
+solves = ['Elvis', 'Ice', 'Game', 'Hunter', 'Tiger'];
 
 
 function clickme()
@@ -8,7 +8,7 @@ function clickme()
 
     if(md5s.includes(value))
     {
-        var index = md5s.indexOf(value); // 1
+        var index = md5s.indexOf(value);
 
 document.getElementById("header").innerHTML = 'Secret Word is: ' + solves[index];
     }
